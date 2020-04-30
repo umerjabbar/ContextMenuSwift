38dfb432d0e67955e06a256231fe4b59e56ecafc
Umer Jabbar <umerabduljabbar@icloud.com>
2020-05-01 02:58:06 +0500

    Initial Commit

---
+++ ContextMenuSwift/ContextMenuSwift.h
@@ -0,0 +1,19 @@
+//
+//  ContextMenuSwift.h
+//  ContextMenuSwift
+//
+//  Created by macmin on 01/05/2020.
+//  Copyright © 2020 Umer jabbar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ContextMenuSwift.
+FOUNDATION_EXPORT double ContextMenuSwiftVersionNumber;
+
+//! Project version string for ContextMenuSwift.
+FOUNDATION_EXPORT const unsigned char ContextMenuSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContextMenuSwift/PublicHeader.h>
+
+
